1e9c3b4e82ae7296f9eb15c1d0eeffbc3eec0054
0penpalms <jamescartermvnyc@gmail.com>
2024-07-25 05:35:16 -0400

    Passed tests

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function decrement(n) {
+    n--
+    return n;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+function increment(n) {
+     n++
+    return n;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+function subtract(a, b) {
+    return a - b;
+}
